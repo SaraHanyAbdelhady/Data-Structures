79cc17ed3b34e92e4a157fcae2a479dcdfbd1c1e
SaraHanyAbdelhady <saraabdelhady80@gmail.com>
2024-12-09 22:01:06 +0200

    Xml_to_Json done bgd  😉😉

---
+++ Xml_to_Json/Xml_to_Json/Xml_to_Json.h
@@ -0,0 +1,19 @@
+#pragma once
+#ifndef _XML_TO_JSON_H_
+#define _XML_TO_JSON_H_
+
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <stack>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void Xml_to_Json(const string& input, const string& output);
+
+#endif // _XML_TO_JSON_H
+
+
