2a1832b4af52c214a743048c15b5eb48b8e2dde6
Amany889900 <140924529+Amany889900@users.noreply.github.com>
2024-12-09 17:57:47 +0200

    isValid Parsing the string debug

--- Validity_checker/isValid.cpp
+++ Validity_checker/isValid.cpp
@@ -4,6 +4,7 @@
 #include <stack>
 #include <vector>
 #include <algorithm>
+#include <queue>
 
 using namespace std;
 
@@ -23,10 +24,58 @@ using namespace std;
         cout << "Unable to open input file: " << input << endl;
         return;
     }
+
+     string inputStream;
+     stack<pair<string,long long>> openings;
+     queue<string> closings;
+     stack<long long> indices;
      
+     int end_of_tag_att = -1; //<Name>Amany</Name>
+    long long counter = 0;
+     while(getline(inputFile,inputStream)){
+        counter++;
+      for(long long i=0; i<inputStream.size();){
+        if(inputStream[i] == '<'  && inputStream[i+1] != '/') {
+            string tagOpen="";
+            i++;
+            while(inputStream[i]!='>' && inputStream[i]!=' '){
+                tagOpen+=inputStream[i];
+                i++;
+            }
+            
+            openings.push(make_pair(tagOpen,counter));
+            i++;
+          
+        }
+        else if(inputStream[i] == '<' && inputStream[i+1] == '/'){
+            string tagClose="";
+            i++;
+            i++;
+            while(inputStream[i]!='>'){
+                tagClose+=inputStream[i];
+                i++;
+            }
+            
+            openings.push(make_pair(tagClose,counter));
+            i++;
+        }
+        else i++;
+      }
+
+          cout<<openings.top().first<<" "<<openings.top().second<<endl;
      
+       
+     }
+
+     inputFile.close();
+     return;
+
 
    
+   }
+
+   int main(){
+    isValid("../sample.xml");
    }
 
   
