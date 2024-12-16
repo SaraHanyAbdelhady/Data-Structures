6f37f11ebe90ffe83ab40c6db7fe9c479e4ae406
MariamHany4 <mar.hany@hotmail.com>
2024-12-16 20:51:19 +0200

    Updating Minifying.cpp

--- Minifying_XML/Minifying.cpp
+++ Minifying_XML/Minifying.cpp
@@ -1,9 +1,11 @@
 #include "Minifying.h"
 int main()
 {
-    ifstream input_file("input_file.xml");
-    ofstream output_file("output_file.xml");
-    Minifying(input_file, output_file);
+    ifstream file1("input_file.xml");
+    ofstream file2("output_file.xml");
+    Minifying(file1, file2);
+    file1.close();
+    file2.close();
     return 0;
 }
 void Minifying(ifstream& input_file, ofstream& output_file) {
@@ -15,20 +17,22 @@ void Minifying(ifstream& input_file, ofstream& output_file) {
     queue<char> result; //queue to store the result to output it in file at the end
     string content_between_tags; //string to preserve the content between tags > and <
     //loop on every character in the file
-    for (size_t i = 0; i < fileContent.size(); ++i) {
-        char c = fileContent[i];
+    for (int i = 0; i < fileContent.length(); i++)
+    {
         //if inside comment so skip it
-        if (in_comment) {
-            if (i + 2 < fileContent.size() && fileContent[i] == '-' && fileContent[i + 1] == '-' && fileContent[i + 2] == '>') {
+        if (in_comment)
+        {
+            if (i + 2 < fileContent.length() && fileContent[i] == '-' && fileContent[i + 1] == '-' && fileContent[i + 2] == '>')
+            {
                 in_comment = false;
                 i += 2; // to skip -->
             }
             continue;
         }
         //if you detect < so you are inside tag
-        if (c == '<') {
-                //if < followed by !-- so its a comment
-            if (i + 3 < fileContent.size() && fileContent[i + 1] == '!' && fileContent[i + 2] == '-' && fileContent[i + 3] == '-')
+        if (fileContent[i] == '<')
+            {
+            if (i + 3 < fileContent.length() && fileContent[i + 1] == '!' && fileContent[i + 2] == '-' && fileContent[i + 3] == '-')
             {
                 in_comment = true;
                 i += 3; // Skip <!--
@@ -38,12 +42,12 @@ void Minifying(ifstream& input_file, ofstream& output_file) {
             // If there was content between tags and it only contains space or \n or \t or \r, remove it
             if (!content_between_tags.empty()) // this condition check if there is anything between tags
                 {
-                    if (all_of(content_between_tags.begin(), content_between_tags.end(), ::isspace)) {
-                        // If only whitespace, don't add it to the result so do nothing
+
+                        // If not only whitespace so add it as it is to result,don't change anything in it
                         //isspace is a function that checks if the string has whitespaces only or not
-                    }
-                    else
-                    { // that means there is non white space characters between tags so add all the content between tags to result as it is with no change
+
+                    if (!(all_of(content_between_tags.begin(), content_between_tags.end(), ::isspace)) )
+                    {
                     for (char ch : content_between_tags)
                         result.push(ch);
 
@@ -53,17 +57,17 @@ void Minifying(ifstream& input_file, ofstream& output_file) {
             //since you detected < so you are between tags
             in_tag = true;
             //add the character to result
-            result.push(c);
+            result.push(fileContent[i]);
         }
         //if you detect > so set in_tag to false and add to result the >
-        else if (c == '>')
+        else if (fileContent[i] == '>')
         {
             in_tag = false;
-            result.push(c);
+            result.push(fileContent[i]);
         }
         //if you are inside tag so do not change anything, we leave what is between tags as it is
         else if (in_tag)
-            result.push(c);
+            result.push(fileContent[i]);
         // if you do not detect < or > and you are not inside tags then you are between tags
         else
         {
@@ -74,15 +78,10 @@ void Minifying(ifstream& input_file, ofstream& output_file) {
             if (next_tag != -1) //not equal -1 means found < because if it didnt find so it returns -1
             {
                 string between_tags = fileContent.substr(i, next_tag - i); //put content between the tags in string between_tags
-                if (all_of(between_tags.begin(), between_tags.end(), ::isspace)) {
-                    // If all content between tags is whitespace, only print the next < and do not print anything before it
-                    if (result.empty() || result.back() != '<') ;
-                }
-                else
+                if(!(all_of(between_tags.begin(), between_tags.end(), ::isspace)) )
                 {
-                    // Otherwise, print all content
-                   for (char ch : between_tags)
-                        result.push(ch);
+                   for (char tt : between_tags)
+                        result.push(tt);
                 }
                 i = next_tag - 1; //skip the content we added to result
             }
@@ -91,7 +90,7 @@ void Minifying(ifstream& input_file, ofstream& output_file) {
     //write result to output file
     while (!result.empty())
     {
-        output_file.put(result.front());
+        output_file<<result.front();
         result.pop();
     }
 }
